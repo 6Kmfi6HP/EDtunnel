000d8a4024be9775cc5a60a4f8b478a0959fb04d
3Kmfi6HP <58669916+3Kmfi6HP@users.noreply.github.com>
2023-06-07 20:24:00 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,8 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 
-let proxyIP = '';
-
+let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 if (!isValidUUID(userID)) {
 	throw new Error('uuid is not valid');
