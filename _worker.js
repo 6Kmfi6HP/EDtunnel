d1f61bb79359cb1757d413d11d010786147444e7
3Kmfi6HP <58669916+3Kmfi6HP@users.noreply.github.com>
2023-07-07 03:11:27 +0700

    Update proxyIP to cdn-all.xn--b6gac.eu.org
    cdn-all.xn--b6gac.eu.org has resolved all known ByoIP.

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 
-let proxyIP = 'cdn.xn--b6gac.eu.org';
+let proxyIP = 'cdn-all.xn--b6gac.eu.org';
 
 let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
 
