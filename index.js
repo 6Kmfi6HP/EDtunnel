67afa412413c4564e7b13851ef0aec9fa6c1a4cc
rkulu <69760842+rkulu@users.noreply.github.com>
2024-12-19 14:19:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ let userID = 'f5d09650-b154-4192-97c5-633ea7717364';
  * Array of proxy server addresses with ports
  * Format: ['hostname:port', 'hostname:port']
  */
-const proxyIPs = ['104.248.145.216,104.26.7.171'];
+const proxyIPs = ['104.248.145.216'];
 
 // Randomly select a proxy server from the pool
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
